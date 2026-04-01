a9dbec2f57d1a49fa2ed4d55d4916f25238f6776
Arpit1922 <arpit.25161049@kiet.edu>
2026-04-01 22:41:25 +0530

    Create EvenNumberstreamapi.java

---
+++ EvenNumberstreamapi.java
@@ -0,0 +1,34 @@
+package streamapi;
+import java.util.*;
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*ArrayList<Integer> list=new ArrayList<>();
+		list.addAll(List.of(10,15,20,25,30,11,17,16));
+		list.stream().filter(n->n%2==0).forEach((n)->System.out.print(n+" "));
+		*/
+		/*ArrayList<String> name=new ArrayList<>();
+		List<String> list=List.of("Amit","Anil","Ravi","Vikas","Ashok","Rahul");
+		name.addAll(list);
+		name.stream().filter(n->n.startsWith("A")).forEach(System.out::println);
+		*/
+	/*	List<Integer> list=Arrays.asList(10,60,45,90,30,24,50,76);
+		ArrayList<Integer> l1=new ArrayList<>();
+		l1.addAll(list);
+		l1.stream().filter(n->n>=50 &&n<=60).forEach(System.out::println);
+		*/
+		/*List<String> list=Arrays.asList("Java","Python","C","React","C#","Next.js","Node.js");
+		ArrayList<String> l1=new ArrayList<>();
+		l1.addAll(list);
+		l1.stream().filter(n->n.length()>4).forEach(System.out::println);
+		*/
+		/*List<String> list=Arrays.asList("Java",null,"Spring",null,"React");
+		ArrayList<String> l1=new ArrayList<>();
+		List<String> notNullList=l1.stream().filter(n->n!=null).toList();
+		System.out.println(notNullList);*/
+		
+	}
+
+		
+}
