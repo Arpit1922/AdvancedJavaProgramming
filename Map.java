de8ceccfdb5b369e51ffc81d095824eeea9c349f
Arpit1922 <arpit.25161049@kiet.edu>
2026-04-06 19:19:57 +0530

    Create Map.java

---
+++ Map.java
@@ -0,0 +1,24 @@
+package streamapi;
+import java.util.*;
+public class MapEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	List<Integer> numbers=Arrays.asList(23,32,45,35,67,89,95);
+		System.out.println(numbers);
+		List<Integer> result=numbers.stream().map(x->x*2).toList();
+		System.out.println(result);*/
+		/* 1. List<String> names=Arrays.asList("sachin","rahul","amit");
+		List<String> result=names.stream().map(x->x.toUpperCase()).toList();
+		System.out.println(result);
+	}*/
+/* 2.  List<Integer> number=Arrays.asList(1,2,3,4);
+ List<Integer> result=number.stream().map(x->x*x).toList();
+ System.out.println(result);*/
+	/*	5. List<String> number=Arrays.asList("A","B","C");
+		List<String> result =new ArrayList<>();
+		result.addAll(number);
+		result.stream().forEach(x->System.out.print("item-"+x+" "));*/
+		
+}
+}
